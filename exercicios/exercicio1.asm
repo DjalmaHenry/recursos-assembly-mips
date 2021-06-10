66f18e53a9ace781b0e99188b93e15c1ed9ff7cc
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-06-10 12:00:46 -0300

    arrumando identação

--- exercicios/exercicio1.asm
+++ exercicios/exercicio1.asm
@@ -10,37 +10,37 @@ Proc1:
 		add $t5, $t5, $a3		#3*j
 		add $t6, $t4, $t5		#[8i+3j]
 		add $t6, $t6, $t6
-		add $t6, $t6, $t6		#achando o endereço 4*[8i+3j]
-		add $t6, $t6, $a1		#novo endereço
-		lw $t2, 0($t6)		#guarda valor de V[8i+3j] em $t2
+		add $t6, $t6, $t6		#achando o endereÃ§o 4*[8i+3j]
+		add $t6, $t6, $a1		#novo endereÃ§o
+		lw $t2, 0($t6)			#guarda valor de V[8i+3j] em $t2
 		add $t7, $a3, $a3
 		add $t7, $t7, $t7		#[4j]
 		add $t7, $t7, $t7
-		add $t7, $t7, $t7		#achando o endereço 4*[4j]
-		add $t7, $t7, $a0		#novo endereço
-		lw $t3, 0($t7)		#guarda valor de M[4j] em $t3
+		add $t7, $t7, $t7		#achando o endereÃ§o 4*[4j]
+		add $t7, $t7, $a0		#novo endereÃ§o
+		lw $t3, 0($t7)			#guarda valor de M[4j] em $t3
 		slt $t4, $t3, $t2		#se M[4j] < V[8i+3j] =1
 		beq $t4, $zero, Exit		#se $t4 == 0, finaliza o loop
-		addi $sp, $sp, -24			#cria espaço na pilha
-		sw $a2, 0($sp)				#salva i na pilha
-		sw $a3, 4($sp)				#salva j na pilha
-		sw $a0, 8($sp)				#salva M[] na pilha
-		sw $a1, 12($sp)			        #salva V[] na pilha
-		sw $ra, 16($sp)			        #salva retorno
-		sw $t7, 20($sp)				#salva M[4j] na pilha
+		addi $sp, $sp, -24		#cria espaÃ§o na pilha
+		sw $a2, 0($sp)			#salva i na pilha
+		sw $a3, 4($sp)			#salva j na pilha
+		sw $a0, 8($sp)			#salva M[] na pilha
+		sw $a1, 12($sp)			#salva V[] na pilha
+		sw $ra, 16($sp)			#salva retorno
+		sw $t7, 20($sp)			#salva M[4j] na pilha
 		add $a2, $t2, $zero		#adiciona valor de $t2 em $a2
 		jal Proc2			#pula para proc2	
-		lw $a3, 4($sp)				#guarda valor de j
-		lw $t7, 20($sp)				#guarda valor de M[4j]
-		sw $v0, 0($t7)		#atualiza valor de M[4j]
+		lw $a3, 4($sp)			#guarda valor de j
+		lw $t7, 20($sp)			#guarda valor de M[4j]
+		sw $v0, 0($t7)			#atualiza valor de M[4j]
 		add $a0, $a3, $zero		#adiciona valor de $a3 em $a0
 		jal Proc3			#pula para proc3
-		lw $a2, 0($sp)				#guarda valor de i
-		lw $a3, 4($sp)				#guarda valor de j
-		lw $a0, 8($sp)				#guarda valor de M[]
-		lw $a1, 12($sp)			        #guarda valor de V[]
-		lw $ra, 16($sp)			        #guarda valor de retorno
-		addi $sp, $sp, 24			#quebra a pilha
+		lw $a2, 0($sp)			#guarda valor de i
+		lw $a3, 4($sp)			#guarda valor de j
+		lw $a0, 8($sp)			#guarda valor de M[]
+		lw $a1, 12($sp)			#guarda valor de V[]
+		lw $ra, 16($sp)			#guarda valor de retorno
+		addi $sp, $sp, 24		#quebra a pilha
 		add $a3, $a2, $v0		#j=i+proc3(j)
 		j Loop				#continua o loop
 	Exit:
