2e070fdfdd7f444dd0fa9b445ade19fcfdc9012a
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-06-13 11:46:53 -0300

    adicionando exercicio 6

---
+++ exercicios/exercicio6.asm
@@ -0,0 +1,38 @@
+# V[]  = $a0
+# Proc1 retorna resultado da média = {$v0}
+Proc1:
+
+	#Soma dos valores
+	addi $t0, $zero, 10			#adiciona 10 a $t0 (limite)
+	add $t1, $zero, $t1 			#adiciona 0 ao $t1 (contador)
+	add $t4, $zero, $t4			#adiciona 0 ao $t1 (soma)
+	Loop:
+		beq $t0, $t1, Exit		#se $t0 == $t1, finaliza o loop
+		add $t2, $zero, $t1		#adiciona index [i]
+		add $t2, $t2, $t2
+		add $t2, $t2, $t2		#achando endereço 4*[i]
+		add $t2, $t2, $a0		#novo endereço
+		lw $t3, 0($t2)			#guarda valor de V[i] em $t3
+		add $t4, $t4, $t3		#soma valores de V[i]
+		addi $t1, $zero, 1 		#adiciona 1 ao $t2 (contador)
+		j Loop				#volta para loop
+	Exit:
+		
+	#Divisão dos valores
+	addi $t0, $zero, 10			#adiciona 10 a $t0 (limite)
+	add $t1, $zero, $t1 			#adiciona 0 ao $t1 (contador)
+	Loop2:
+		beq $t0, $t1, Exit2		#se $t0 == $t1, finaliza o loop
+		slt $t5, $t1, $t0			#se proc3 < proc2 | 1= menor que, 0= maior que ou igual
+		add $t2, $zero, $t1		#adiciona index [i]
+		add $t2, $t2, $t2
+		add $t2, $t2, $t2		#achando endereço 4*[i]
+		add $t2, $t2, $a0		#novo endereço
+		lw $t3, 0($t2)			#guarda valor de V[i] em $t3
+		add $t4, $t4, $t3		#soma valores de V[i]
+		addi $t1, $zero, 1 		#adiciona 1 ao $t2 (contador)
+		j Loop2				#volta para loop2
+	Exit2:
+		add $v0, $zero, $t2		#adiciona $t2 a $v0
+		jr $ra				#retorna $ra
+		
