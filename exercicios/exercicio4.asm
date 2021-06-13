16ca0f2f9fcac0d28c63a4dd9e99acff95e7855f
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-06-13 18:18:59 -0300

    arrumando identação

--- exercicios/exercicio4.asm
+++ exercicios/exercicio4.asm
@@ -4,12 +4,12 @@
 #While(Save[i] != Test[j])
 Loop:
 	add $t0, $t0, $a2
-	add $t0, $t0, $t0			#achando endereço 4*i
-	add $t0, $t0, $a0			#novo endereço de Save[i]
+	add $t0, $t0, $t0			#achando endereÃ§o 4*i
+	add $t0, $t0, $a0			#novo endereÃ§o de Save[i]
 	lw $t1, 0($t0)				#guarda valor de Save[i] em $t1
 	add $t2, $t2, $a3
-	add $t2, $t2, $t2			#achando endereço 4*j
-	add $t2, $t2, $a1			#novo endereço de Test[j]
+	add $t2, $t2, $t2			#achando endereÃ§o 4*j
+	add $t2, $t2, $a1			#novo endereÃ§o de Test[j]
 	lw $t3, 0($t2)				#guarda valor de Test[j] em $t3
 	beq $t1, $t3, Exit			#se Save[i] == Test[j], finaliza o loop
 	beq $a2, $a3, If			#i == j, chama o if
@@ -21,8 +21,8 @@ Loop:
 If:
 	addi $t4, $a2, 1			#i+1
 	add $t4, $t4, $t4
-	add $t4, $t4, $t4			#achando endereço 4*i
-	add $t4, $t4, $a0			#novo endereço de Save[i+1]
+	add $t4, $t4, $t4			#achando endereÃ§o 4*i
+	add $t4, $t4, $a0			#novo endereÃ§o de Save[i+1]
 	lw $t5, 0($t4)				#guarda valor de Save[i+1] em $t5
 	addi $t5, $t5, 2			#$t5 = Save[i+1]+2
 	sw $t5, 0($t0)				#Save[ i ] = $t5
@@ -32,10 +32,10 @@ If:
 Else:
 	addi $t4, $a2, 1			#i+1
 	add $t4, $t4, $t4
-	add $t4, $t4, $t4			#achando endereço 4*i
-	add $t4, $t4, $a0			#novo endereço de Save[i+1]
+	add $t4, $t4, $t4			#achando endereÃ§o 4*i
+	add $t4, $t4, $a0			#novo endereÃ§o de Save[i+1]
 	lw $t5, 0($t4)				#guarda valor de Save[i+1] em $t5
-	subi $t5, $t5, 2				#$t5 = Save[i+1]-2
+	subi $t5, $t5, 2			#$t5 = Save[i+1]-2
 	sw $t5, 0($t0)				#Save[ i ] = $t5
 	jr $ra					#retorna
 
