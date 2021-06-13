6001d658f0b096cae4d4951d78a2dcf2b1e9011f
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-06-13 15:00:47 -0300

    arrumando identação

--- exercicios/exercicio6.asm
+++ exercicios/exercicio6.asm
@@ -1,5 +1,5 @@
 # V[]  = $a0
-# Proc1 retorna resultado da mÈdia = {$v0}
+# Proc1 retorna resultado da m√©dia = {$v0}
 Proc1:
 
 	#Soma dos valores
@@ -10,19 +10,19 @@ Proc1:
 		beq $t0, $t1, Exit		#se $t0 == $t1, finaliza o loop
 		add $t2, $zero, $t1		#adiciona index [i]
 		add $t2, $t2, $t2
-		add $t2, $t2, $t2		#achando endereÁo 4*[i]
-		add $t2, $t2, $a0		#novo endereÁo
+		add $t2, $t2, $t2		#achando endere√ßo 4*[i]
+		add $t2, $t2, $a0		#novo endere√ßo
 		lw $t3, 0($t2)			#guarda valor de V[i] em $t3
 		add $t4, $t4, $t3		#soma valores de V[i]
 		addi $t1, $zero, 1 		#adiciona 1 ao $t2 (contador)
 		j Loop				#volta para loop
 	Exit:
 		
-	#Divis„o dos valores
+	#Divis√£o dos valores
 	addi $t0, $zero, 10			#adiciona 10 a $t0 (limite e divisor)
 	add $t1, $zero, $t1 			#adiciona 0 ao $t1 (contador e resultado)
 	Loop2:
-		beq $t4, $zero, Exit2	#se $t4 == 0, finaliza o loop
+		beq $t4, $zero, Exit2		#se $t4 == 0, finaliza o loop
 		sub $t4, $t4, $t0		#soma - 10
 		addi $t1, $zero, 1 		#adiciona 1 ao $t1 (contador)
 		j Loop2				#volta para loop2
